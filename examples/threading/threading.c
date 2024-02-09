38e8449ffa0e2e758b8d8cb9a411bc1078ec9bd7
vsrlinux <vsrlinux@ubuntu.myguest.virtualbox.org>
2024-02-09 08:57:15 +0530

    Added_test_conditions

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -11,23 +11,37 @@
 
 void* threadfunc(void* thread_param)
 {
-    printf("HHHHHHHHHHHHHHHHHHHHHH\n");
     struct thread_data* thread_func_args = (struct thread_data *)thread_param;
-    sleep((thread_func_args->wait_to_obtain_ms)/1000);
-    pthread_mutex_lock(thread_func_args->mutex);
-    sleep((thread_func_args->wait_to_release_ms)/1000);
-    printf("LLLLLLLLLLLLLLLLLLLLLLLLLLL\n");
-    pthread_mutex_unlock(thread_func_args->mutex);
-    printf("OOOOOOOOOOOOOOOOOOOOOOOOOOO\n");
+    unsigned int sleepval = sleep((thread_func_args->wait_to_obtain_ms)/1000);
+    if(sleepval !=0)
+    {
+        ERROR_LOG("Sleep incomplete");
+        return thread_param;
+
+    }
+    int lckval = pthread_mutex_lock(thread_func_args->mutex);
+    if(lckval !=0)
+    {
+        ERROR_LOG("Locking failed");
+        return thread_param;
+    }
+    sleepval = sleep((thread_func_args->wait_to_release_ms)/1000);
+    if(sleepval !=0)
+    {
+        ERROR_LOG("Sleep incomplete");
+        return thread_param;
+    }
+    int unlock_val = pthread_mutex_unlock(thread_func_args->mutex);
+    if(unlock_val !=0)
+    {
+        ERROR_LOG("Unlock failed");
+        return thread_param;
+    }
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
     thread_func_args->thread_complete_success = true;
     return thread_param;
-        // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    // typecasting (void *)thread param to struct thread_data*
-
 }
 
 
@@ -52,6 +66,7 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
     {
         errno = rc;
         perror("pthread_create");
+        free(new_data);
         new_data->thread_complete_success = false;
         return false;
     }
