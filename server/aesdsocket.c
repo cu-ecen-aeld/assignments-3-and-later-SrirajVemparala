0ba74177f40315ff097a392986f960dbf0845a24
vsrlinux <vsrlinux@vsrlinux.myguest.virtualbox.org>
2024-02-24 15:58:21 -0700

    issuewithmalloc

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -130,21 +130,27 @@ int main(int argc, char* argv[])
     //Rec messages
     addr_size = sizeof(their_addr);
     memset(rec_val,0,REC_LEN);
-    char *store_data = (char*)malloc(sizeof(char)*REC_LEN);
-    if(store_data == NULL)
-    {
-        syslog(LOG_ERR,"Unable to allocate memory");
-        printf("malloc failed\n");
-        exit(1);
-    }
-    else
-    {
-        syslog(LOG_DEBUG,"Successfully_created_file");
-        printf("malloc_succ\n");
-    }
     uint32_t total_length = 0;
+    //
+    //
+    //Start of accepting the connections
+    //
+    //
     while(1)
-    {       
+    {      
+        //malloc performed
+        char *store_data = (char*)malloc(sizeof(char)*REC_LEN);
+        if(store_data == NULL)
+        {
+            syslog(LOG_ERR,"Unable to allocate memory");
+            printf("malloc failed\n");
+            exit(1);
+        }
+        else
+        {
+            syslog(LOG_DEBUG,"Successfully_created_file");
+            printf("malloc_succ\n");
+        }
         sock_accept_fd = accept(sock_fd, (struct sockaddr *)&their_addr, &addr_size);
         if(sock_accept_fd == -1)
         {
@@ -159,7 +165,7 @@ int main(int argc, char* argv[])
             syslog(LOG_INFO, "Accepts connection from %s", inet_ntoa(((struct sockaddr_in*)&their_addr)->sin_addr));
         }
         int recv_len = 0;
-        //total_length = 0;
+        
         while (accepted)
         {
             printf("In_ACCEPT\n");
@@ -178,46 +184,54 @@ int main(int argc, char* argv[])
             {
                 printf("In recvlen 0\n");
                 accepted = 0;
+                total_length = 0;
                 break;
             }           
-            while(i <= recv_len)
+            while(i < recv_len)
             {
                 if(rec_val[i] == '\n')
                 {
                     push_data = 1;
+                    i++;
                     break;
                 }
                 i++;    
             }
-            i++;
+            //i++;
             printf("i is:%d\n",i);
-            printf("length_is:%d\n",total_length);
+            //printf("length_is:%d\n",total_length);
+            
                 if(REC_LEN - total_length >= i)
                 {
                     memcpy(store_data+total_length,rec_val,i);   
                 }
                 else
                 {
-                    //Handling
+                    printf("SFERRRRRRRRRRRRRRRRRRRRRRRR\n");
+                    printf("i_val:%d\n",i);
+                    printf("tot+i:%d\n",total_length+i);
+                    //Handling                                      10 + 128
                     char* store_data = (char*)realloc(store_data,total_length+i);
                     memcpy(store_data+total_length,rec_val,recv_len);
                 }
                 total_length+=i;
-                for(int j = 0; j < total_length;j++)
-                {
-                    printf("data[%d]:%x\n",j,store_data[j]);
-                }
-                i = 0;
+                // for(int j = 0; j < total_length;j++)
+                // {
+                //     printf("data[%d]:%x\n",j,store_data[j]);
+                // }
+                
                 memset(rec_val,0,recv_len);
             if(recv_len !=0 && push_data == 1)
             {
+                //Tried Read, write but it did not work becasue once written teh pointer inth efile points to the last location and hence no data is read
                 //My data is stored in the buffer now write it to the file file_aesdsocket
-                file_fd = open(file_aesdsocket,O_APPEND|O_RDWR);
+                file_fd = open(file_aesdsocket,O_WRONLY|O_APPEND);
                 if(file_fd == -1)
                 {
                     printf("File opening failed\n");
                     exit(1);
                 }
+                printf("length is %d\n",total_length);
                 int nr = write(file_fd, store_data, total_length);
                 if(nr == -1)
 	            {
@@ -243,23 +257,35 @@ int main(int argc, char* argv[])
 		        	    exit(1);
 		            }
 	            }
+                close(file_fd);
+
                 file_length+=total_length;
+                printf("%d:file_length\n",file_length);
                 //total_length = 0;
                 char file_data[file_length];
+                file_fd = open(file_aesdsocket,O_RDONLY);
+                if(file_fd == -1)
+                {
+                    printf("File opening failed\n");
+                    exit(1);
+                }
                 int rd = read(file_fd,file_data,file_length);
                 if(rd == -1)
                 {
                     syslog(LOG_ERR,"unable to read data");
                     printf("read_error\n");
+                    close(file_fd);
                 }
+                close(file_fd);
                 int data_sent = 0,data_ptr_inc= 0,length_tobe_sent = file_length;
                 while(data_sent_flag == 0)
                 {       
-                    data_sent = send(sock_accept_fd, store_data+data_ptr_inc, length_tobe_sent, 0);
+                    data_sent = send(sock_accept_fd, file_data+data_ptr_inc, length_tobe_sent, 0);
                     if(data_sent == -1)
                     {
                         syslog(LOG_ERR,"Error in sending the data");
                         printf("Send data failed\n");
+                        
                     }
                     else if(data_sent < file_length)
                     {
@@ -275,7 +301,7 @@ int main(int argc, char* argv[])
                 data_sent_flag  = 0;
                 push_data = 0;
                 close(file_fd);
-		        //free(store_data);
+		        free(store_data);
 		        syslog(LOG_ERR, "Closed connection with %s\n", inet_ntoa(((struct sockaddr_in*)&their_addr)->sin_addr));
                 printf("Closed connection with %s\n", inet_ntoa(((struct sockaddr_in*)&their_addr)->sin_addr));
             }
